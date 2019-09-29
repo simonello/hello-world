61c576c8ee04d95d627b7e7890a2cdf1e69b17c7
Simon Smith <simon.r.smith@gmail.com>
2019-09-29 10:59:29 +0100

    update to numerics test program.

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all: hello.c
 	gcc -o hello hello.c -I.
+	gcc -o num num.c -I.
 
 clean:
-	rm -f hello
+	rm -f hello num
---
+++ num.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+int main()  
+{
+	int x;
+	for (x=0; x<10; x++)
+		{
+		printf("%d,",x); 
+		}
+	printf("\n"); 
+	return(0); 
+}
